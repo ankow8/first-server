447ff42bf3496137177d2d3adeea34efe9e1be78
Antoni Kowalewski <kowalewski.antek@gmail.com>
2022-01-18 22:30:33 +0100

    Add express-handlebars

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const path = require('path');
+const hbs = require('express-handlebars');
 
 const app = express();
+
+app.engine('.hbs', hbs());
+app.set('view engine', '.hbs');
 
 app.use((req, res, next) => {
   res.show = (name) => {
@@ -10,25 +14,39 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use(express.static(path.join(__dirname, ('/public'))));
+
 app.get('/', (req, res) => {
-  res.show('index.html');
+  res.render('index', {layout: false});
 });
 
 app.get('/about', (req, res) => {
-  res.show('about.html');
+  res.render('about', {layout: false});
 });
 
 app.get('/contact', (req, res) => {
-  res.show('contact.html');
+  res.render('contact', {layout: false});
 });
 
 app.get('/info', (req, res) => {
-  res.show('info.html');
+  res.render('info', {layout: false});
 });
 
 app.get('/history', (req, res) => {
-  res.show('history.html');
+  res.render('history', {layout: false});
 });
+
+app.get('/hello/:name', (req, res) => {
+  res.render('Hello', {layout: false, name: req.params.name});
+});
+
+/*app.get('/style.css', (req, res) => {
+  res.sendFile(path.join(__dirname, '/style.css'));
+});
+
+app.get('/test.png', (req, res) => {
+  res.sendFile(path.join(__dirname, '/test.png'));
+});*/
 
 app.use((req, res) => {
   res.status(404).send('404 not found...');
